7e18cf6476255d0baab84bc7e18275251894574d
jerry <digitsu@gmail.com>
2022-03-05 14:38:21 +0900

    Added users additions and field validations

--- mix.exs
+++ mix.exs
@@ -20,7 +20,7 @@ defmodule Rumbl.MixProject do
   def application do
     [
       mod: {Rumbl.Application, []},
-      extra_applications: [:logger, :runtime_tools]
+      extra_applications: [:logger, :runtime_tools, :os_mon]
     ]
   end
 
@@ -36,6 +36,8 @@ defmodule Rumbl.MixProject do
       {:phoenix, "~> 1.6.5"},
       {:phoenix_ecto, "~> 4.4"},
       {:ecto_sql, "~> 3.6"},
+      {:ecto_psql_extras, "~> 0.6"},
+      {:pbkdf2_elixir, "~> 1.0"},
       {:postgrex, ">= 0.0.0"},
       {:phoenix_html, "~> 3.0"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
